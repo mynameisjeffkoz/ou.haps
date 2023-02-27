abef35f2f88b89083f0e55c10c78c0ac36ecf956
Jeffrey Kozlowski <jeffrey.kozlowski@me.com>
2023-02-27 17:08:43 -0600

    Fix sink rate and double check glide time

--- HAPS_glideTime.m
+++ HAPS_glideTime.m
@@ -1,9 +1,9 @@
-function t = HAPS_glideTime(hi, hf, w0)
+function t = HAPS_glideTime(hi, hf, m0)
 % Calculate the glide time beteween two altitudes for the HAPS at a given
 % weight
 t = 0;
 resolution = .1;
 for h=hi:-resolution:hf+resolution
-    Vv = HAPS_sinkRate(h, w0);
+    Vv = HAPS_sinkRate(h, m0);
     t = t + resolution/Vv;
 end
--- HAPS_sinkRate.m
+++ HAPS_sinkRate.m
@@ -1,4 +1,4 @@
-function Vv = HAPS_sinkRate(altitude, w0)
+function Vv = HAPS_sinkRate(altitude, m0)
 % Calculate the sink rate of the unpowered HAPS aircraft at the given
 % altitude and weight
 g = 9.80665;
@@ -9,8 +9,8 @@ k = 1 / (pi*e*AR);
 Cl_max = 1.7;
 S = 58.8;
 rho = atmosDensity(altitude);
-V = 1.2 * HAPS_stallSpeed(altitude, w0);
+V = 1.2 * HAPS_stallSpeed(altitude, m0);
 q = 0.5 * rho * V^2;
-CL = w0 / (q * S);
+CL = m0 * g / (q * S);
 CD =  CD0 + k * CL^2;
-Vv = sqrt(w0/S * 2/rho * CD^2/CL^3);
+Vv = sqrt(m0 * g/S * 2/rho * CD^2/CL^3);
