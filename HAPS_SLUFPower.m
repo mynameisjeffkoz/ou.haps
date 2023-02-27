c8181f2860fddf4b5b32ff4f243e5c4ca0a32be1
Jeffrey Kozlowski <jeffrey.kozlowski@me.com>
2023-02-27 17:01:08 -0600

    Fix SLUF power calculation

--- HAPS_SLUFPower.m
+++ HAPS_SLUFPower.m
@@ -1,4 +1,4 @@
-function P = HAPS_SLUFPower(alt, w0)
+function P = HAPS_SLUFPower(alt, m0)
 % Calculate the usable power requirement for SLUF at a given altitude and weight
 g = 9.80665;
 AR = 30;
@@ -7,9 +7,9 @@ e = 0.8;
 k = 1 / (pi*e*AR);
 S = 58.8;
 rho = atmosDensity(alt);
-V = 1.2 * HAPS_stallSpeed(alt, w0);
+V = 1.2 * HAPS_stallSpeed(alt, m0);
 q = 0.5 * rho * V^2;
-CL = w0 / (q * S);
+CL = m0 * g / (q * S);
 CD =  CD0 + k * CL^2;
 D = q * S * CD;
 P = D * V;
