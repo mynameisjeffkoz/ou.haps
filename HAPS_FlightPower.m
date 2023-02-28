1c82864259e883ed9a5840dc0708abf4dea0b316
Jeffrey Kozlowski <jeffrey.kozlowski@me.com>
2023-02-28 16:08:12 -0600

    Fix labeling of input arguments

--- HAPS_FlightPower.m
+++ HAPS_FlightPower.m
@@ -1,13 +1,13 @@
-function P = HAPS_FlightPower(condition, alt, w0,S)
+function P = HAPS_FlightPower(condition, alt, m0,S)
 % Calculate the total power draw at flight conditions, inlcuding system and
 % payload power usage.
 e_prop = 0.8;
 e_motor = 0.95;
 e_overall = e_prop * e_motor;
 if (condition == "SLUF")
-    P = HAPS_SLUFPower(alt,w0,S) / e_overall + HAPS_IdleDraw();
+    P = HAPS_SLUFPower(alt,m0,S) / e_overall + HAPS_IdleDraw();
 elseif (condition == "CLIMB")
-    P = w0 * .04 * 1000 / e_overall + HAPS_IdleDraw();
+    P = m0 * .04 * 1000 / e_overall + HAPS_IdleDraw();
 elseif (condition == "GLIDE")
     P = HAPS_IdleDraw();
 end
