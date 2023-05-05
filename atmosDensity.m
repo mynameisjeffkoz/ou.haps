cd89aa816c5105bf7477c2c71fd1b01a41aef736
Jeffrey Kozlowski <jeffrey.kozlowski@me.com>
2023-05-05 16:28:59 -0500

    Density calculation now works below msl

--- atmosDensity.m
+++ atmosDensity.m
@@ -8,11 +8,15 @@ T_b = [288.15 216.65 216.65 228.65 270.65 270.65 214.65];
 h_b = [0 11000 20000 32000 47000 51000 71000 100000];
 L_b = [-.0065 0 0.001 0.0028 0 -0.0028 -0.002];
 % Assume minimum density altitude of 0
+%{
 if h < 0
     h = 0;
 end
+%}
 for i = 1:(length(h_b) - 1)
-    if h >= h_b(i) & h < h_b(i + 1)
+    if h < 0
+        break
+    elseif h >= h_b(i) & h < h_b(i + 1)
         break
     end
 end
